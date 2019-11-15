4f855c40cf055465b47739fdcd04ea18fb5db1b8
ZhenJie <cn.zhenjie@gmail.com>
2019-11-15 20:55:47 +0800

    finish the main function ,now it's a useable version

--- background.js
+++ background.js
@@ -1,15 +1,116 @@
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({ color: '#3aa757' }, function () {
-        console.log("The color is green.");
-    });
+const MAX_LEAVES = 1300;
+//flag that specify if it's day time 
+let isDay = true;
+//the amount of leaves
+let leaves = 30;
+let fall = 0;
+let notificationId = 0;
+//the reverse-amount of water
+let water = 0;
+let min_count = 0;
+//the text in right_top corner
+let text_1 = "^_^";
+//the text in the bottom 
+let text_2;
+let noti_timer = 5;
 
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: 'developer.chrome.com' },
-            })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
+function timer() {
+    if (!isDay) {
+        return;
+    }
+    min_count++;
+    water++;
+    if (min_count >= 6) {
+        text_1 = "I NEED WATER";
+        leaves -= water - 6;
+        fall += water - 6;
+        chrome.browserAction.setBadgeText({ text: "DRY" });
+        chrome.browserAction.setBadgeBackgroundColor({ color: "#f7941d" });
+        if (noti_timer % 5 == 0) {
+            sendNotification()
+        }
+        noti_timer++;
+
+    }
+    if (min_count < 6) {
+        chrome.browserAction.setBadgeText({ text: min_count + "" });
+        chrome.browserAction.setBadgeBackgroundColor({ color: "#7cc576" });
+        if (min_count < 6 && min_count > 4) {
+            text_1 = "*_*";
+        } else if (min_count <= 4 && min_count > 2) {
+            text_1 = "-_-";
+        } else {
+            text_1 = "^_^";
+        }
+    }
+}
+
+function sendNotification() {
+    chrome.notifications.clear("1",
+        (id) => {
+
+        });
+    chrome.notifications.create("1", {
+        type: 'basic',
+        iconUrl: './images/mint_icon128',
+        title: 'NEED WATER',
+        message: 'water the mint tree, and get yourself a cup of water'
     });
-});
+}
+
+
+
+function waterTheTree() {
+    min_count = 0;
+    water = 0;
+    fall = 0;
+    leaves += 30;
+    timer();
+    min_count--;
+    water++;
+    if (leaves > MAX_LEAVES) {
+        leaves = 0;
+        text_1 = "THE TREE HAS BEEN RESETTED"
+    }
+}
+
+/**
+ * belows are the getter and setter
+ */
+function getText1() {
+    return text_1;
+}
+
+function setText1(txt) {
+    text_1 = txt;
+}
+
+function getText2() {
+    return text_2;
+}
+
+function setText2(txt) {
+    text_2 = txt;
+}
+
+function getIsDay() {
+    return isDay;
+}
+
+function getLeaves() {
+    return leaves;
+}
+
+function getFall() {
+    return fall;
+}
+
+function switchDay() {
+    isDay = !isDay;
+}
+
+
+min_count--;
+water--;
+timer();
+setInterval(timer, 60000);
--- popup.js
+++ popup.js
@@ -1,5 +1,83 @@
-let changeColor = document.getElementById("changeColor");
-chrome.storage.sync.get("color",(data)=>{
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value',data.color);
+let bg = chrome.extension.getBackgroundPage();
+let container = document.getElementsByClassName("pop_container")[0];
+let sunOrMoon = document.getElementById("sun_or_moon");
+let watering_can = document.getElementById("watering_can");
+let leaves = document.getElementById("leaves");
+let fall = document.getElementById("fall");
+let isDay = bg.isDay;
+let leaves_canvas = document.getElementById("leaves_canvas").getContext('2d');
+let fall_canvas = document.getElementById("fall_canvas").getContext('2d');
+//the colors for the leaves
+let leaves_colors = ["#7cc576","#c4df9b","#39b54a","#3cb878"];
+let fall_colors = ["#f7941d","#ece484"];
+let leaves_count;
+let fall_count;
+let info_text = document.getElementById("info_text");
+let low_text = document.getElementById("low_text")
+
+refresh();
+
+
+sunOrMoon.addEventListener("click",()=>{
+    bg.switchDay();
+    refresh();
 });
+
+watering_can.addEventListener("click",()=>{
+    if(bg.getIsDay()){
+    bg.waterTheTree();
+    refresh();
+    info_text.innerHTML = "THE TREE GOT WATER<br/>^_^"
+    }
+});
+
+//re-performing the layout each time when data changed
+function refresh(){
+    //clear the canvas
+    leaves_canvas.clearRect(0,0,160,120);
+    fall_canvas.clearRect(0,0,200,60);
+    //justify if it's day time
+    isDay = bg.getIsDay();
+    //get the number of leaves on tree and the fall on ground 
+    leaves_count = bg.getLeaves();
+    fall_count = bg.getFall();
+    //switch the background according to 'isDay'
+    if(!isDay){
+        container.classList.add("night");
+        sunOrMoon.classList.add("moon");
+        info_text.classList.add("night");
+        watering_can.classList.add("night");
+        info_text.innerHTML="MINT TREE IS SLEEPING. . . NO WATER NEEDED ! !"
+    }else{
+        container.classList.remove("night");
+        info_text.classList.remove("night");
+        sunOrMoon.classList.remove("moon");
+        watering_can.classList.remove("night");
+        info_text.innerHTML=bg.getText1();
+    }
+    //draw the leaves on the tree
+    for(i=0;i<leaves_count;i++){
+        leaves_canvas.fillStyle = leaves_colors[Math.floor((Math.random()*4+1)-1)];
+        leaves_canvas.beginPath();
+        let x = Math.floor(Math.random()*160);
+        let y = Math.floor(Math.random()*120);
+        let r = Math.floor(Math.random()*5);
+        leaves_canvas.arc(x,y,r,0,2*Math.PI);
+        leaves_canvas.fill();
+    }
+    //draw the fall on the ground 
+    for(j=0;j<fall_count;j++){
+        fall_canvas.fillStyle = fall_colors[Math.floor((Math.random()*2+1)-1)];
+        fall_canvas.beginPath();
+        let x = Math.floor(Math.random()*200);
+        let y = Math.floor(Math.random()*60);
+        let r = Math.floor(Math.random()*5);
+        fall_canvas.arc(x,y,r,0,2*Math.PI);
+        fall_canvas.fill();
+    }
+}
+
+
+
+
+
